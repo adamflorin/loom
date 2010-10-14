b699bb4fc035de233ecf91fcdda0dd888f3bfccb
adamflorin <adamflorin@gmail.com>
2010-10-14 21:04:28 +0000

    light rfctr

--- ruby/music_loom/gestures/bucephalus.rb
+++ ruby/music_loom/gestures/bucephalus.rb
@@ -6,29 +6,27 @@
 module MusicLoom
   class Bucephalus < Gesture
     
+    LONG_DURATION = 480
+    NUM_STEPS = 20
+    
     # set up a note to play faster & faster
     # 
     def generate_events(now)
       events = []
+      event_time = next_beat(now)
       
-      start_time = (now / 480).ceil * 480
-      event_time = start_time
-
-      long_dur = 480
-      steps = 20
-
       # TODO: add a pitch bend continuous event here
-
-      steps.times do |i|
+      
+      NUM_STEPS.times do |i|
         # > 0. and <= 1.0
-        pcnt = (steps - (i+1)).to_f / steps
-
+        pcnt = (NUM_STEPS - (i+1)).to_f / NUM_STEPS
+        
         # exponential for effect
-        delta_ms = long_dur * (pcnt ** 2.0)
-
+        delta_ms = LONG_DURATION * (pcnt ** 2.0)
+        
         # push onto the queue
         events << [event_time, ["note", 59.9, 100, 100]]
-
+        
         event_time += delta_ms
       end
       
