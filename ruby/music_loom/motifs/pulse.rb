2ad75a987e61c86215b035b7186f87f73100290d
Adam Florin <adamflorin@gmail.com>
2011-10-27 17:32:51 -0700

    make steps and timescale configurable in pulse! (with deviation of course)

--- ruby/music_loom/motifs/pulse.rb
+++ ruby/music_loom/motifs/pulse.rb
@@ -6,21 +6,27 @@
 module MusicLoom
   class Pulse < Motif
     
+    # because enum'd live.dials only output the INDEX of the dial! silly!
+    TIMESCALE_VALUES = [TICKS_64N, TICKS_32N, TICKS_16N, TICKS_8N, TICKS_4N, TICKS_2N, TICKS_1N]    
+    
     # Emit one single note
     # 
-    # TODO: input: # of steps
     # TODO: input: velocity
-    # TODO: timescale
     # 
     def generate_gesture(now)
       Gesture.new(Motif::next_beat(now)) do |gesture|
-        duration = TICKS_4N
+        event_time = 0
+        timescale_index = parameter(:timescale).round
+        rate = TIMESCALE_VALUES[timescale_index.constrain(0..TIMESCALE_VALUES.size-1)]
         
-        gesture.make :note, :at => 0, :data => {
-          :pitch => parameter(:pitch),
-          :duration => duration}
+        parameter(:steps).to_i.constrain(0..16).times do |i|
+          gesture.make :note, :at => event_time, :data => {
+            :pitch => parameter(:pitch),
+            :duration => rate}
+          event_time += rate
+        end
 
-        gesture.make :done, :at => duration
+        gesture.make :done, :at => event_time
       end
     end
     
