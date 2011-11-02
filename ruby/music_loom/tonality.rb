59dee2b4687f6eb80e17a230d0a2b2837e84fba2
Adam Florin <adamflorin@gmail.com>
2011-11-01 18:49:07 -0700

    tune world to C

--- ruby/music_loom/tonality.rb
+++ ruby/music_loom/tonality.rb
@@ -11,7 +11,7 @@ module MusicLoom
     CONFIG_DIR = File.join(File.dirname(__FILE__), '..', '..', 'config')
     
     # tune the world to C
-    BASE_PITCH = 64
+    BASE_PITCH = 60
     
     attr :scales, :scale_id
     
